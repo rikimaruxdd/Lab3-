9433cef2033720d5a84667986858576315df635d
gojyb <mohammadahmadi3@cmail.carleton.ca>
2024-09-21 23:30:50 -0400

    commit

---
+++ Lab1/src/AddressBook.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+public class AddressBook {
+    private BuddyInfo buddy;
+    private ArrayList<BuddyInfo> buddies;
+
+
+    public AddressBook() {
+
+        this.buddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        if(buddy != null) {
+            buddies.add(buddy);
+        }
+    }
+    public BuddyInfo removeBuddy(int index) {
+
+        if (index >= 0 && index < buddies.size()) {
+            return buddies.remove(index);
+        }
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+
+        BuddyInfo buddy = new BuddyInfo("Mohammad",
+                "1600 Pennsylvania Avenue",
+                "999");
+        System.out.println("Hello " + buddy.getName());
+
+
+
+    }
+}
