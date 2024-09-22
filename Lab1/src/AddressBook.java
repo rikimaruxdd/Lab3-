f9544f6782e0395817b00f6547c7815cec6ad3ea
gojyb <mohammadahmadi3@cmail.carleton.ca>
2024-09-21 23:33:03 -0400

    commit3

--- Lab1/src/AddressBook.java
+++ Lab1/src/AddressBook.java
@@ -35,5 +35,6 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(0);
+        System.out.println("lab3");
     }
 }
